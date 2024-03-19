a4c9c8a99e6bcfdd42c565f3606a580bfc47686e
notpure12 <ravananot@gmail.com>
2024-03-19 15:11:05 +0700

    added live demo

--- src/assets/myProjects.jsx
+++ src/assets/myProjects.jsx
@@ -1,12 +1,14 @@
 export const myProjects = [
 {
-    url:'https://github.com/notpure12/Todolist',
+    git:'https://github.com/notpure12/Todolist',
+    url:'',
     pic:'https://i.postimg.cc/P5qXt9Ch/todolist-project.jpg',
     title:'TODOLIST CRUD',
     tech:[' Nest.js, ','mySQL, ','React, ','Vite'],
 },
 {
-    url:'',
+    git:'https://github.com/notpure12/Netflix-clone/',
+    url:'https://notpure12.github.io/Netflix-clone/',
     pic:'https://i.postimg.cc/020WS0Ct/Netflix.jpg',
     title:'Netflix Clone',
     tech:[' HTML, ','CSS, ','Java Script '],
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -15,7 +15,7 @@ export const Projects = () => {
         {myProjects.map((myProjects) => (
           <>
             <div className="p-5 grid border-black ">
-              <a href={myProjects.url} target="_blank" className="cursur-pointer">
+              <a href={myProjects.git} target="_blank" className="cursur-pointer">
                 <img src={myProjects.pic} className="w-[100%] hover:scale-95 transition-all duration-500"></img>
               </a>
               <h1 className="font-customFontSemibold text-xl">
@@ -24,15 +24,17 @@ export const Projects = () => {
               <p className="text-base md:text-lg">
                 Tech used :{myProjects.tech}
               </p>
-              <div>
-                <a className="flex gap-5" href={myProjects.url} target="_blank">
+              <div className="flex gap-5">
+                <a href={myProjects.git} target="_blank">
                   <button className="flex gap-1 items-center justify-center bg-black text-white w-24 h-6 border border-black rounded-sm hover:bg-white hover:text-black py-2 px-2">
                     <FaGithub /> GITHUB
                   </button>
+                  </a>
+                  <a href={myProjects.url} target="_blank">
                   <button className="flex gap-1 items-center justify-center bg-black text-white w-32 h-6 border rounded border-black rounded-sm hover:bg-white hover:text-black  py-2 px-2">
                     <FaPlay className="text-sm" /> LIVE DEMO
                   </button>
-                </a>
+                  </a>
               </div>
             </div>
           </>
